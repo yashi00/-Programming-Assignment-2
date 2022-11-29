d4eae79c0313d216b10b341621c7038c98c8997b
Yashi Srivastava <72307197+yashi00@users.noreply.github.com>
2022-11-29 23:33:24 +0530

    Add files via upload

---
+++ cachematrix.R
@@ -0,0 +1,32 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+
+## Write a short comment describing this function
+setwd('C:\Users\avira\OneDrive\Documents\GitHub\ProgrammingAssignment2')
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
+##
+## Same here, changed "mean" to "solve" and "m" to "s"
+cacheSolve <- function(x, ...) {
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting inversed matrix")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
+}
